81f3616cf0fbf2627ebc89504036da52f4f51637
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-09-12 17:06:49 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -92,3 +92,5 @@ $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
 
 # Call the Leica Camera setup
 $(call inherit-product-if-exists, vendor/xiaomi/mojito-leicacamera/mojito-leicacamera-vendor.mk)
+# Disable unwanted apps
+PRODUCT_PACKAGES -= Calendar
